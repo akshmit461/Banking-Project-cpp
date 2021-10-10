3a7268ca2314fa233cb5437210175d04d8fd8dcc
akshmit461 <70439200+akshmit461@users.noreply.github.com>
2021-10-10 16:18:48 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,27 +29,26 @@ using namespace std;
 
 //Class Bank is declared
 class Bank	{
-
-    //Private data members are declared to store account information
-    char accountHolderName[30],address[100];
-    char typeOfAccount;
-    float accountBalance;
-    int accountNumber;
-    int withdrawCount=0;
-    //All Member functions are declared
-    public:
-    void feeddata();
-    void editdata();
-    void showdata();
-    int search(int );
-    void deposit(float);
-    void withdraw(float);
-    float balance();
-    char type();
-    void records();
-    void setcount();
-    int getcount();
-    float calcInterest();
+	//Private data members are declared to store account information
+	char accountHolderName[30],address[100];
+	char typeOfAccount;
+    	float accountBalance;
+    	int accountNumber;
+    	int withdrawCount=0;
+	    //All Member functions are declared
+	public:
+		void feeddata();
+		void editdata();
+		void showdata();
+		int search(int );
+		void deposit(float);
+		void withdraw(float);
+		float balance();
+		char type();
+		void records();
+		void setcount();
+		int getcount();
+		float calcInterest();
 	
 };
 
@@ -57,17 +56,17 @@ class Bank	{
 void Bank::feeddata()	{
 	cin.ignore();
 	cout<<"\nEnter Account Holder Name: ";      
-  cin.getline(accountHolderName,30);
+  	cin.getline(accountHolderName,30);
 	cout<<"Enter type of account (C/S): ";
-  cin>>typeOfAccount;
-  typeOfAccount=toupper(typeOfAccount);
-  cout<<"Enter account number: ";
+  	cin>>typeOfAccount;
+  	typeOfAccount=toupper(typeOfAccount);
+  	cout<<"Enter account number: ";
 	cin>>accountNumber;
-  cin.ignore();
-  cout<<"Enter accoun holder's address: ";
+  	cin.ignore();
+  	cout<<"Enter accoun holder's address: ";
 	cin.getline(address,100);
-  cout<<"Enter account balance: ";   
-  cin>>accountBalance; 
+  	cout<<"Enter account balance: ";   
+  	cin>>accountBalance; 
 	
 }
 
@@ -75,23 +74,23 @@ void Bank::feeddata()	{
 void Bank::editdata()	{
 	cin.ignore();
 	cout<<"\nEnter Account Holder Name: ";      
-  cin.getline(accountHolderName,30);
+  	cin.getline(accountHolderName,30);
 	cout<<"Enter type of account (C/S): ";
-  cin>>typeOfAccount;
-  typeOfAccount=toupper(typeOfAccount);
-  cout<<"Enter account number: ";
+  	cin>>typeOfAccount;
+  	typeOfAccount=toupper(typeOfAccount);
+  	cout<<"Enter account number: ";
 	cin>>accountNumber;
-  cin.ignore();
-  cout<<"Enter accoun holder's address: ";
+  	cin.ignore();
+  	cout<<"Enter accoun holder's address: ";
 	cin.getline(address,100);
-  cout<<"Enter account balance: ";   
-  cin>>accountBalance; 
+  	cout<<"Enter account balance: ";   
+  	cin>>accountBalance; 
 }
 
 //defined to display account information 
 void Bank::showdata()	{
 	cout<<"\nAccount Holder's name: "<<accountHolderName;
-  cout<<"\nAccount Holder's address: "<<address;
+  	cout<<"\nAccount Holder's address: "<<address;
 	cout<<"\nAccount type: "<<typeOfAccount;
 	cout<<"\nAccount number: "<<accountNumber;
 	cout<<"\nAccount balance: "<<accountBalance;   
@@ -108,22 +107,22 @@ int Bank::search(int accNumber )	{
 
 //defined to deposit amount in an account
 void Bank :: deposit(float amount){
-  accountBalance+=amount;
+	accountBalance+=amount;
 }
 
 //defined to withdraw amount from an account
 void Bank :: withdraw(float amount){
-  accountBalance-=amount;
+  	accountBalance-=amount;
 }
 
 //defined to return bank account balance 
 float Bank:: balance(){
-  return accountBalance;
+  	return accountBalance;
 }
 
 //defined to return type of account
 char Bank::type(){
-  return typeOfAccount;
+  	return typeOfAccount;
 }
 
 //defined to print all records
@@ -133,26 +132,26 @@ void Bank::records(){
 
 //defined to maintain withdraw count
 void Bank:: setcount(){
-  withdrawCount++;
+  	withdrawCount++;
 }
 
 //defined to return withdraw count
 int Bank:: getcount(){
-  return withdrawCount;
+  	return withdrawCount;
 }
 
 //deined to calculate intetest depending upon withdrw count
 float Bank::calcInterest(){
-  float interest;
-  if(withdrawCount<3){
-    float rate=5;
-    interest=accountBalance*rate*1/100;
-  }
-  else{
-    float rate=3.5;
-    interest=accountBalance*rate*1/100;
-  }
-  return interest;
+	float interest;
+	if(withdrawCount<3){
+    		float rate=5;
+    		interest=accountBalance*rate*1/100;
+  	}
+  	else{
+    		float rate=3.5;
+    		interest=accountBalance*rate*1/100;
+  	}
+  	return interest;
 }
 
 //Functions are declared
@@ -164,101 +163,100 @@ void displayRecord(); //to display all the records
 void closeAccount(int); //to close a account
 void interest(int); //to calculate interest
 
-int main()	{
-
-  //variable i maintains the count of entries, t is used in loops, choice is declared to know the operation to be performed
+int main(){
+	
+	//variable i maintains the count of entries, t is used in loops, choice is declared to know the operation to be performed
 	int i=0,t,choice;
+	
+	//declared to enter account number which helps in searching the particular account in records
+	int accNumber;
 
-  //declared to enter account number which helps in searching the particular account in records
-  int accNumber;
-
-  //amount to be withdrwan OR amount to be deposited
-  float amount;
+	//amount to be withdrwan OR amount to be deposited
+	float amount;
 
 	while(1)	{
-    //All instructions are dispalyed on screen. User can perform any operation out of the following instructions
+    	//All instructions are dispalyed on screen. User can perform any operation out of the following instructions
 		cout<<"\n\n\t\tMENU"
 		<<"\n1. Entry of New account"
 		<<"\n2. Deposit Amount"
-    <<"\n3. Withdraw Amount"
+    		<<"\n3. Withdraw Amount"
 		<<"\n4. Search For Details"
 		<<"\n5. Edit Details Of existing account"
-    <<"\n6. Display all accounts"
-    <<"\n7. Close account"
-    <<"\n8. Calculate interest"
+		<<"\n6. Display all accounts"
+		<<"\n7. Close account"
+		<<"\n8. Calculate interest"
 		<<"\n9. Exit"
 		<<"\n\nEnter your Choice: ";
 		cin>>choice;//entering choice input
 		
-    //switch to select one operation out of all according to the choice made by user
+    		//switch to select one operation out of all according to the choice made by user
 		switch(choice)	{
+				//entry of new account
+				case 1:
+					newAccount();
+					break;
 
-      //entry of new account
-			case 1:
-        newAccount();
-        break;
+		      		//Deposit amount
+				case 2:
+					cout<<"\nEnter Account Number: ";
+					cin>>accNumber;
+					updateBalance(accNumber, 1);
+					break;
 
-      //Deposit amount
-			case 2:
-        cout<<"\nEnter Account Number: ";
-        cin>>accNumber;
-        updateBalance(accNumber, 1);
-        break;
+		      		//Withdraw amount
+		      		case 3:
+					cout<<"\nEnter Account Number: ";
+					cin>>accNumber;
+					updateBalance(accNumber, 2);
+					break;
 
-      //Withdraw amount
-      case 3:
-        cout<<"\nEnter Account Number: ";
-        cin>>accNumber;
-        updateBalance(accNumber, 2);
-        break;
+		      		//show data  
+				case 4: 
+					cin.ignore();
+					cout<<"\nEnter Account Number: ";
+					cin>>accNumber;
+					//If account exist--->>Show the account information
+					displayAccDetails(accNumber);
+					break;
+				
+		      		//edit account information
+				case 5: 
+					cin.ignore();
+					cout<<"\nEnter Account Number: ";
+					cin>>accNumber;
+					modifyAccount(accNumber);
+					break;
 
-      //show data  
-			case 4: 
-        cin.ignore();
-				cout<<"\nEnter Account Number: ";
-        cin>>accNumber;
-        //If account exist--->>Show the account information
-        displayAccDetails(accNumber);
-				break;
-			
-      //edit account information
-			case 5: 
-        cin.ignore();
-				cout<<"\nEnter Account Number: ";
-        cin>>accNumber;
-				modifyAccount(accNumber);
-				break;
+		      		//Display all the records
+		      		case 6: 
+					displayRecord();
+					break;
 
-      //Display all the records
-      case 6: 
-        displayRecord();
-        break;
-			
-      //close a account
-      case 7:
-        cout<<"\nEnter Account Number: ";
-        cin>>accNumber;
-        closeAccount(accNumber);
-        break;
+		      		//close a account
+		     		case 7:
+					cout<<"\nEnter Account Number: ";
+					cin>>accNumber;
+					closeAccount(accNumber);
+					break;
 
-      //calculate interest for a account
-      case 8:
-        cout<<"\nEnter Account Number: ";
-        cin>>accNumber;
-        interest(accNumber);
-        break;
+		      		//calculate interest for a account
+		      		case 8:
+					cout<<"\nEnter Account Number: ";
+					cin>>accNumber;
+					interest(accNumber);
+					break;
 
-      //exit the program
-			case 9: 
-        exit(0);
+		      		//exit the program
+		      		case 9: 
+					exit(0);
 
-      //default choice
-			default: 
-        cout<<"\nInvalid Choice Entered";
+		      		//default choice
+		      		default: 
+					cout<<"\nInvalid Choice Entered";
+				}
+			}
+			return 0;
 		}
-	}
-	return 0;
-}
 
 //create new account
 void newAccount()
@@ -305,9 +303,10 @@ void updateBalance(int n, int option)
 				int bal=obc.balance()-amt;
 				if((bal<500 && obc.type()=='S') || (bal<1000 && obc.type()=='C'))
 					cout<<"Insufficience balance";
-				else
+				else{
 					obc.withdraw(amt);
-          obc.setcount();
+         				obc.setcount();
+				}
 			}
 			int pos=(-1)*static_cast<int>(sizeof(obc));
 			File.seekp(pos,ios::cur);
@@ -316,9 +315,9 @@ void updateBalance(int n, int option)
 			found=true;
 	       }
          }
-	File.close();
-	if(found==false)
-		cout<<"\n\n Record Not Found ";
+			File.close();
+			if(found==false)
+				cout<<"\n\n Record Not Found ";
 
 }
 
@@ -337,7 +336,7 @@ void displayAccDetails(int n)
 	}
 	cout<<"\nBALANCE DETAILS\n";
   
-  while(inFile.read(reinterpret_cast<char *> (&obc), sizeof(obc)))
+  	while(inFile.read(reinterpret_cast<char *> (&obc), sizeof(obc)))
 	{
 		if(obc.search(n))
 		{
